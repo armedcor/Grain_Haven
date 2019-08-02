f506f34c81ad99b88475ad5cf145215e28a78ee9
Cormac Lawlor <armedcor@gmail.com>
2019-08-02 14:52:21 +0000

    Added home page route and recipe route

--- app.py
+++ app.py
@@ -4,20 +4,27 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+# Configure Flask App and set app Variables
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'brewing_database'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+# Landing Page Route
+
 
 @app.route('/')
+def home_page():
+    return render_template('home.html')
+
+# Get all recipes
+
+
 @app.route('/get_recipes')
 def get_recipes():
     return render_template('recipes.html', recipes=mongo.db.recipes.find())
-    
-
-
 
 
 if __name__ == '__main__':
