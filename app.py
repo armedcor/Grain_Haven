423bdef0ef67d79edb0b1a4ef930f1b537cc3ce1
Cormac Lawlor <armedcor@gmail.com>
2019-08-02 08:48:07 +0000

    linked up MongoDB and started templating

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'brewing_database'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    
+
 
 
 
